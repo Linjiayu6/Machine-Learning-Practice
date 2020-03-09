313f13f740ba600682561369aaac97ac86bb0e95
林佳钰 <linjiayu@meituan.com>
2020-03-09 21:33:06 +0800

    fix

--- logistic_regression/demo_horse_colic/horse_colic.py
+++ logistic_regression/demo_horse_colic/horse_colic.py
@@ -4,9 +4,9 @@ import math
 import numpy as np
 # import matplotlib.pyplot as plt
 
-def loadData ():
+def loadData (txt):
     # 训练集合载入
-    fr = open('./data/trainingset.txt')
+    fr = open(txt)
     X, y_arr = [], []
     for linestr in fr.readlines():
         lineArr = linestr.strip().split()
@@ -23,24 +23,34 @@ def sigmoid (z):
     # sigmoid function
     return 1 / (1 + np.exp(-z))
 
-def classifer (z):
+def classifer (X, y_arr, weights):
     # 分类器: z = X * w
-    probability = sigmoid(z)
-    if probability >= 0.5:
-        return 1
-    else:
-        return 0
+    z = X * weights
+    y_predict = np.array(sigmoid(z)).T[0]
+    m = len(y_arr)
+    error = []
+    for i in range(m):
+        if y_predict[i] >= 0.5:
+            if y_arr[i] != 1:
+                error.append(i)
+        else:
+            if y_arr[i] != 0:
+                error.append(i)
+    return float(len(error)) / float(m)
 
 def train (X, y_arr):
     # 0. init
-    alpha = 0.01
-    loop = 100
+    # alpha = 0.01
+    loop = 100000
 
     # 1. weights
     m, n = X.shape # m = 299 data, n = 22 features [w0, w1, ...]
     weights = np.ones((n, 1))
     
     for i in range(loop):
+        # alpha = 0.01 + 3 / (i + 1)
+        alpha = 0.001 + 3 / (i + 1)
+
         # 2. z = X * weights = sum( w1x1 + w2x2 + ... ) => z = (299 * 22) * (22 * 1) = (299 * 1)
         z = X * weights
         
@@ -63,14 +73,37 @@ def evaluate (X, y_arr, weights):
         y_1 = - np.mat(y_arr).T
         fn_1 = np.log2(y_predict)
         y_2 = - np.mat((1 - y_arr)).T
+        # 遇到log(0) 负无穷大情况, 补0 ???
         fn_2 = np.log2(1 - y_predict)
         return np.multiply(np.array(y_1), np.array(fn_1)) + np.multiply(np.array(y_2), np.array(fn_2))
     
-    print(costfunction(y_arr, y_predict)[:5])
-    J = costfunction(y_arr, y_predict) / y_arr.shape[0]
-    # print('Successful Rate:', (1 - sum(np.array(J).T) / len(y_arr)) * 100, '%')
+    J = costfunction(y_arr, y_predict)
+    # inf情况 infinite, 补0
+    inf_index = np.isinf(J)
+    J[inf_index] = 0
+    J = J / y_arr.shape[0]
 
-X, y_arr = loadData()
-weights = train(X, y_arr)
+    rate = (1 - J.sum() / len(y_arr)) * 100
+    print('Successful Rate:', rate, '%')
 
-evaluate(X, y_arr, weights)
+if __name__ == "__main__":
+    # (1) load training set
+    X, y_arr = loadData('./data/trainingset.txt')
+    
+    # (2) train
+    weights = train(X, y_arr)
+    
+    # (3) evaluate costfn by training set
+    evaluate(X, y_arr, weights)
+    
+    # (4) evaluate costfn by test set
+    X_test, y_test_arr = loadData('./data/testset.txt')
+    evaluate(X_test, y_test_arr, weights)
+    
+    # (5) error rate
+    err_rate = classifer(X, y_arr, weights)
+    print('Training dataset: error rate', err_rate)
+
+    err_rate = classifer(X_test, y_test_arr, weights)
+    print('Test dataset: error rate', err_rate)
+    
